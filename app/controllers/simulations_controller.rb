d38e99990a375fd7cf73d9d1234ea745b6795515
trebek1 <trebek08@gmail.com>
2015-07-30 16:57:46 -0700

    Added variables and tested that they provided the right type of structure

--- app/controllers/simulations_controller.rb
+++ app/controllers/simulations_controller.rb
@@ -1,6 +1,29 @@
 class SimulationsController < ApplicationController
   def index
-  	@data = File.read("poker.txt").gsub(/\s+/, "").scan(/.{10}/)
+  	@data = File.read("poker.txt").gsub(/\s+/, "").scan(/.{10}/) # Data from file removing dashes
+  	# and making into 10 piece increments so that each one is a "hand" for either player 1 or 2 
+
+  	@hands1 = Array.new # player 1 and player 2 hands arrays ( just the cards from the file)
+  	@hands2 = Array.new
+  	@player1hands = Hash.new #player hands with calculated information about combinations
+    @player2hands = Hash.new
+    @info = Hash.new # info about the given hand in the hand_results function
+    @eachinfo = Hash.new # object with all info for all hands (for given player) in hand results function   
+    
+    #Store best hand numerical value 
+    @top1 = Array.new 
+    @top2 = Array.new
+
+    #best hand in card value 
+    @best1 = Array.new
+    @best2 = Array.new    
+
+    #number of wins for player 1 and player 2 
+    @wins1 = 0
+    @wins2 = 0
+
+    @highcardwinner1 = Array.new 
+    @highcardwinner2 = Array.new
 
   end
 end
--- spec/controllers/simulations_controller_spec.rb
+++ spec/controllers/simulations_controller_spec.rb
@@ -6,4 +6,23 @@ RSpec.describe SimulationsController do
       get :index
       expect(response).to render_template("index")
     end
-end
+
+    it "loads 2000 hands of poker of length 10" do
+    	data = File.read("poker.txt").gsub(/\s+/, "").scan(/.{10}/)
+    	expect(data.length).to equal 2000
+    end 	
+    
+	it "Should create a new hash" do
+    	@player1hands = Hash.new 
+     	Hash.new.should == {}
+     end 
+	it "Should create a new array" do
+    	@hands1 = Array.new 
+     	Array.new.should == []
+     end 
+     it "should equal zero when set to zero" do 
+     	@wins1 = 0
+     	@wins1.should eql(0)
+     end 
+ 	
+end 
