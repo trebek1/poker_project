da3eb0259828f9b7b61164b9a04f8f75b77a9059
trebek1 <trebek08@gmail.com>
2015-07-30 19:18:26 -0700

    added pair logic and valued the pair at 20. Also included tests for verification

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -67,12 +67,12 @@ RSpec.describe SimulationsController do
  	expect(max).to equal 13
  	end # end case  	
  	
- 	it "finds the max value and assigns max for the hand to that value (numeric)" do
+ 	it "finds the max value and assigns max for the hand to that value (numeric), then stores in object" do
 	    examplehand = '7C3S3C3H3S'
 	    value = 0
 	    max = 0
 	    @info = {}
-	    
+
 	    for char in 0..examplehand.length-1
 		    if char %2 == 0  # First character is the number (starting index 0)
 		    	card = examplehand[char]
@@ -114,6 +114,33 @@ RSpec.describe SimulationsController do
  	expect(@info['max']).to equal 7
  	end # end case 
 
+ 	it " should test positive for a pair (and ignore the suits) and set value equal to 20" do 
+	 	@top = 0
+	 	@info = {}
+	 	@info['pair'] = 0
+ 		@info = {'H' => 2, 'S' => 2, 'D' => 2, 'C' => 2, '4' =>2, 'pair'=> 0}
+ 		@info.each do |key,val|
+		 	if @info[key] ==2 || @info[key] == 3
+				# if 3 of a kind, we dont want to test for pair so we look for 3kind first 
+				if @info[key] == 3 && key != 'H' && key != 'S' && key != 'D' && key != 'C' && key != 'max' && key != 'pair'
+					@info['3kind'] =1
+					# see if 3kind is best hand, if so make max value 
+					if @top < 50
+						@top = 50
+					end 
+					# test for pair if not a 3 of a kind 
+				elsif @info[key] == 2 && key != 'H' && key != 'C' && key != 'max' && key != 'S' && key != 'D' && key != 'pair'
+					@info['pair'] +=1
+					# see if pair is best hand, if so, set max to value 
+					if @top < 20* @info['pair']
+						@top = 20*@info['pair']
+					end 
+				end	 
+			end 
+		end 
+		expect(@top).to eql 20
+		expect(@info['pair']).to eql 1
+	end 
 
 
 end # end spec (controller)
