e9c374f23cae91b0890fb8ad46be5cf79ea36c3d
trebek1 <trebek08@gmail.com>
2015-07-30 21:45:22 -0700

    added view spec and view

--- spec/views/index_spec.rb
+++ spec/views/index_spec.rb
@@ -1,1 +1,16 @@
 require "rails_helper"
+
+RSpec.describe 'views/simulations/index.html.erb' do
+
+	it "expects wins in increment given a win (player 1)" do
+		@top1 = [10]
+		@top2 = [5]
+		@wins1 = 0
+
+		if @top1[0] > @top2[0]
+			@wins1+=1 
+		end 
+
+		expect(@wins1).to eql 1
+	end 
+end 	
